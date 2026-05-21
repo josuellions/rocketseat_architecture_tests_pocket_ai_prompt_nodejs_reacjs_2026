d51bc43414335b01fb0bee21d475bea247bf3475
josuellions <josuellions@gmail.com>
2026-05-21 20:08:17 -0300

    chore: setup and config design system.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import {Button} from '@/components/ui/button';
+
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
@@ -6,6 +8,7 @@ export default function Home() {
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
             Hello word - new project - arctheture and tests
           </h1>
+          <Button variant="outline">New button</Button>
         </div>
       </main>
     </div>
