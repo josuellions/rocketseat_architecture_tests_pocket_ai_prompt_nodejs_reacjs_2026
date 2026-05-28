cd6b1a98bc982a9d346cc8eead2ec385fded8f14
josuellions <josuellions@gmail.com>
2026-05-28 19:59:37 -0300

    test:  create test `prompt-list`.

--- src/components/prompts/prompt-list.tsx
+++ src/components/prompts/prompt-list.tsx
@@ -1,9 +1,10 @@
 import { PromptSummary } from '@/core/domain/prompts/prompt.entity';
 import { PromptCard } from './prompt-card';
 
-type PromptListProps = {
+export type PromptListProps = {
   prompts: PromptSummary[];
 };
+
 export const PromptList = ({ prompts }: PromptListProps) => {
   return (
     <ul className="space-y-2">
--- src/tests/components/prompts/prompt-list.spec.tsx
+++ src/tests/components/prompts/prompt-list.spec.tsx
@@ -1,3 +1,32 @@
+import { PromptList, type PromptListProps } from '@/components/prompts';
+import { render, screen } from '@/lib/test-utils';
+
+const makeSut = ({ prompts }: PromptListProps) => {
+  return render(<PromptList prompts={prompts} />);
+};
+
 describe('PromptList', () => {
-  it('should create a new prompt successfully', async () => {});
+  it('should render the list of prompts', async () => {
+    const prompts = [
+      { id: '1', title: 'Title 01', content: 'Content 01' },
+      { id: '2', title: 'Title 02', content: 'Content 02' },
+      { id: '3', title: 'Title 03', content: 'Content 03' },
+    ];
+
+    makeSut({ prompts });
+
+    expect(screen.getByRole('list')).toBeInTheDocument();
+    expect(screen.getAllByRole('listitem')).toHaveLength(3);
+    expect(screen.getByText(prompts[0].title)).toBeInTheDocument();
+    expect(screen.getByText(prompts[1].title)).toBeInTheDocument();
+    expect(screen.getByText(prompts[2].title)).toBeInTheDocument();
+  });
+  it('should render ab empty list when the array prompt is empty ', async () => {
+    const prompts = [] as PromptListProps['prompts'];
+
+    makeSut({ prompts });
+
+    expect(screen.getByRole('list')).toBeInTheDocument();
+    expect(screen.queryAllByRole('listitem')).toHaveLength(0);
+  });
 });
